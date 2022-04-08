76552b562832bacb4c56d31972e12d791cd87d1e
nikkathehope <nikkathehope@gmail.com>
2022-04-08 15:52:36 +0900

    useTabs

--- src/App.js
+++ src/App.js
@@ -1,62 +1,40 @@
 import { useState } from "react";
 
-const useInput = ( initialValue, validator ) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = event => {
-    const {
-      target: {value}
-    } = event;
-    let willUpdate = true;
-    if(typeof validator === "function"){
-      willUpdate = validator(value);
-    }
-    if(willUpdate){
-      setValue(value);
-    }
+const content = [
+  {
+    tab:"Section 1",
+    content:"I'm the content of the Section 1"
+  },
+  {
+    tab:"Section 2",
+    content:"I'm the content of the Section 2"
   }
-  return { value, onChange };
+];
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  if(!allTabs || !Array.isArray(allTabs)){
+    return;
+  }
+
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
 };
 
 const App = () => {
-  const maxLen = (value) => value.length < 10;
-  const name = useInput("Mr.", maxLen);
-
+  const {currentItem, changeItem} = useTabs(0, content);
+  
   return (
     <div className="App">
       <h1>Hey</h1>
-      <input placeholder="Name" type="text" {...name}/>
+      {content.map((section, i) => (
+        <button onClick={() => changeItem(i)} key={section.tab}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>      
     </div>
   );
 }
-/*
-class AppUgly extends React.Component {
-  state ={
-    item: 1
-  }
-  render(){
-    const { item } = this.state;
-    return(
-      <div className="App">
-        <h1>Hey {item}</h1>
-        <button onClick={this.incrementItem}>Increment</button>
-        <button onClick={this.decrementItem}>Decrement</button>
-      </div>
-    );
-  }
-  incrementItem = () => {
-    this.setState(state => {
-      return {
-        item: state.item + 1
-      }
-    })
-  }
-  decrementItem = () => {
-    this.setState(state => {
-      return {
-        item: state.item - 1
-      }
-    })
-  }
-}
-*/
+
 export default App;
